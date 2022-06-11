b8c8c5d3a47411c03d103b549fe054f0be44cb02
Chakka Ranga Nandan Sresty <62930476+nandan916-s@users.noreply.github.com>
2022-06-11 20:03:59 +0530

    Update actions.py

--- Chatbot/actions/actions.py
+++ Chatbot/actions/actions.py
@@ -1,26 +1,30 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
 
+import logging
+from typing import Any, Text, Dict, List
+import os
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import re
+import string
 
-# This is a simple example for a custom action which utters "Hello World!"
+logging.basicConfig(level=logging.DEBUG)
 
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.forms import FormAction
+class ActionGreet(Action):
+    def name(self):
+        return "action_PN_2_save_name"
 
-# class ActionHelloWorld(Action):
-
-#      def name(self) -> Text:
-#          return "action_hello_world"
-
-#      def run(self, dispatcher: CollectingDispatcher,
-#              tracker: Tracker,
-#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#          dispatcher.utter_message(text="Hello World!")
-
-#          return []
+    def run(self, dispatcher, tracker, domain):
+        intros = ["im ", "i am ", "i am called ", "im called ", "call me "]
+        name = ""
+        stripped = ""
+        sentence = (tracker.latest_message)['text']
+        exclude = set(string.punctuation)
+        stripped = stripped.join(ch for ch in sentence if ch not in exclude).lower()
+        for intro in intros:
+            if re.search(intro, stripped):
+                name = stripped.split(intro)[1]
+                name = name.title()
+        if name is "":
+            name = sentence.title()
+        logging.debug("*** ActionGreet: Name saved as: " + name)
+        return [SlotSet("name", name)]
